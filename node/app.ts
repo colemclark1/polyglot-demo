6c33f089cff7551b951bd5b36a0fe5ed9c33270d
Kovit Nisar <kovit.nisar@ibm.com>
2023-03-16 13:00:55 -0400

    adding remaining routes

--- node/app.ts
+++ node/app.ts
@@ -2,8 +2,29 @@ import express, { Express, Request, Response } from 'express'
 
 const app: Express = express()
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/hello-world', (req: Request, res: Response) => {
     res.send("Hello World");
+});
+
+// get all users
+app.get('/users', (req: Request, res: Response) => {
+    // returns all users
+});
+
+// get user by id
+app.get('/user/:id', (req: Request, res: Response) => {
+    res.send("User details requested for user id " + req.params.id)
+});
+
+// add new user
+app.post('/user', (req: Request, res: Response) => {
+    const user = req.body;
+    res.send("created user " + user)
+});
+
+// updates user by id
+app.put('/user/:id', (req: Request, res: Response) => {
+    res.send("Updated User details for user id " + req.params.id)
 });
 
 app.listen('8080', () => {
